ffa80d6194fa51a0b6327f94447d5858d10056ef
Antoine Demblon <ademblon@oasis.uclouvain.be>
2020-03-21 17:12:14 +0100

    Add Animation & back stack clear after login

--- Wishlist/app/src/main/java/com/example/wishlist/LoginActivity.java
+++ Wishlist/app/src/main/java/com/example/wishlist/LoginActivity.java
@@ -26,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
     public void openMainActivity()
     {
         Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 }
--- Wishlist/app/src/main/java/com/example/wishlist/WishlistsFragment.java
+++ Wishlist/app/src/main/java/com/example/wishlist/WishlistsFragment.java
@@ -21,6 +21,7 @@ public class WishlistsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_wishlists, container, false);
     }
 }
