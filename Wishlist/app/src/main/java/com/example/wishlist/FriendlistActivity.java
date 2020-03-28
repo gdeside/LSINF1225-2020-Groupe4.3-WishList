afe232bad54e031a5015c7c2107bfc8c2d063d11
Antoine Demblon <ademblon@oasis.uclouvain.be>
2020-03-28 15:12:26 +0100

    Add Bottom Navigation Menu

--- Wishlist/app/src/main/java/com/example/wishlist/FriendlistActivity.java
+++ Wishlist/app/src/main/java/com/example/wishlist/FriendlistActivity.java
@@ -1,8 +1,13 @@
 package com.example.wishlist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class FriendlistActivity extends AppCompatActivity {
 
@@ -10,6 +15,38 @@ public class FriendlistActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friendlist);
+
+        //Initalize and Assign Bottom Navigation View
+        BottomNavigationView navigationView = findViewById(R.id.bottomNavigationView);
+
+        //Set Friendlist Button Selected
+        navigationView.setSelectedItemId(R.id.bottomNavFriendlist_btn);
+
+        ///Perform ItemSelected
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()){
+                    case R.id.bottomNavFriendlist_btn:
+                        //startActivity(new Intent(getApplicationContext(),FriendlistActivity.class));
+                        //overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Animation entre écran
+                        return true;
+                    case R.id.bottomNavProfile_btn:
+                        startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Animation entre écran
+                        return true;
+                    case R.id.bottomNavHome_btn :
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Animation entre écran
+                        return true;
+                    case R.id.bottomNavWishlists_btn :
+                        startActivity(new Intent(getApplicationContext(),WishlistsActivity.class));
+                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Animation entre écran
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override  //required for back animation
--- Wishlist/app/src/main/java/com/example/wishlist/MainActivity.java
+++ Wishlist/app/src/main/java/com/example/wishlist/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.wishlist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
 
     private View.OnClickListener Profile_listener = new View.OnClickListener() {
         @Override
@@ -30,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +47,42 @@ public class MainActivity extends AppCompatActivity {
         wishlists_btn.setOnClickListener(Wishlists_listener);
         profile_btn.setOnClickListener(Profile_listener);
         friendlist_btn.setOnClickListener(Friendlist_listener);
+
+        //Initalize and Assign Bottom Navigation View
+        BottomNavigationView navigationView = findViewById(R.id.bottomNavigationView);
+
+        //Set Home Button Selected
+        navigationView.setSelectedItemId(R.id.bottomNavHome_btn);
+
+        ///Perform ItemSelected
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId()){
+                    case R.id.bottomNavFriendlist_btn:
+                        startActivity(new Intent(getApplicationContext(),FriendlistActivity.class));
+                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Animation entre écran
+                        return true;
+                    case R.id.bottomNavProfile_btn:
+                        startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Animation entre écran
+                        return true;
+                    case R.id.bottomNavHome_btn :
+                        //startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        //overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Animation entre écran
+                        return true;
+                    case R.id.bottomNavWishlists_btn :
+                        startActivity(new Intent(getApplicationContext(),WishlistsActivity.class));
+                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Animation entre écran
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
+
+
 
     @Override
     public void finish() {
