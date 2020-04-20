dbfa5b8868d9623890eb61ee81a8bebe6011457b
gdeside <guillaume.deside@student.uclouvain.be>
2020-04-20 18:34:58 +0200

    update log_in

---
+++ Wishlist/app/src/main/java/com/example/wishlist/create_group.java
@@ -0,0 +1,14 @@
+package com.example.wishlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create_group extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+    }
+}
