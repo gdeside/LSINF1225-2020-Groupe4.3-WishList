21a43fb8740988a0b37ffd24f40be5c0414e6b31
gdeside <guillaume.deside@student.uclouvain.be>
2020-04-20 21:09:06 +0200

    update create_group

--- Wishlist/app/src/main/java/com/example/wishlist/create_group.java
+++ Wishlist/app/src/main/java/com/example/wishlist/create_group.java
@@ -3,12 +3,21 @@ package com.example.wishlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class create_group extends AppCompatActivity {
+
+    Button btn_creatgr;
+    EditText name_group;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
+
+
+        btn_creatgr=findViewById(R.id.btn_creategr);
+        name_group=findViewById(R.id.creategr_name);
     }
 }
