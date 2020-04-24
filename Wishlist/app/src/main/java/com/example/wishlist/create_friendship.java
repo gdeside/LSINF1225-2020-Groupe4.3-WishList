2217c47998cdb71c362bf23f2fe4b4182e73887c
gdeside <guillaume.deside@student.uclouvain.be>
2020-04-24 12:22:02 +0200

    add link profil to updateprofile

--- Wishlist/app/src/main/java/com/example/wishlist/create_friendship.java
+++ Wishlist/app/src/main/java/com/example/wishlist/create_friendship.java
@@ -1,14 +1,23 @@
 package com.example.wishlist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class create_friendship extends AppCompatActivity {
     Button btn_friendship;
     EditText friendship_nickname;
+
+    private View.OnClickListener create_friendship_listener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(),"Please fills Details",Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +28,6 @@ public class create_friendship extends AppCompatActivity {
         //éléments layout
         btn_friendship=findViewById(R.id.createfd_btn);
         friendship_nickname=findViewById(R.id.createfd_nickname);
+        btn_friendship.setOnClickListener(create_friendship_listener);
     }
 }
