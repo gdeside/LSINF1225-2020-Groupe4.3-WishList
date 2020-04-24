f281f5dfa2647aa911a7e73fa8055c0b11984642
gdeside <guillaume.deside@student.uclouvain.be>
2020-04-24 12:38:17 +0200

    add search friend

--- Wishlist/app/src/main/java/com/example/wishlist/create_friendship.java
+++ Wishlist/app/src/main/java/com/example/wishlist/create_friendship.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class create_friendship extends AppCompatActivity {
+
+    SearchView searchfriend;
     Button btn_friendship;
     EditText friendship_nickname;
 
@@ -28,6 +31,8 @@ public class create_friendship extends AppCompatActivity {
         //éléments layout
         btn_friendship=findViewById(R.id.createfd_btn);
         friendship_nickname=findViewById(R.id.createfd_nickname);
+        searchfriend=findViewById(R.id.search_friend);
         btn_friendship.setOnClickListener(create_friendship_listener);
+
     }
 }
