2fe47fbcb47cc8ca8eab7a1d12183fe8f22944f6
gdeside <guillaume.deside@student.uclouvain.be>
2020-04-29 17:54:41 +0200

    update delete profile

--- Wishlist/app/src/main/java/com/example/wishlist/UpdateProfileActivity.java
+++ Wishlist/app/src/main/java/com/example/wishlist/UpdateProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.wishlist;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.wishlist.Classesapp.ListAndUserRepository;
 import com.example.wishlist.Classesapp.User;
 import com.example.wishlist.Classesapp.UserRepository;
 
@@ -68,7 +71,21 @@ public class UpdateProfileActivity extends AppCompatActivity {
         up_surname = (EditText) findViewById(R.id.edt_update_surname);
         up_password = (EditText) findViewById(R.id.edt_update_password);
         up_date=(EditText) findViewById(R.id.edt_update_DateofBirth);
-        delete_profile=findViewById(R.id.edit_deleteprofile);
+        delete_profile=(ImageView)findViewById(R.id.edit_deleteprofile);
+
+        delete_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                New_id = up_id.getText().toString().trim();
+                New_password = up_password.getText().toString();
+                New_name = up_name.getText().toString().trim();
+                New_surname = up_surname.getText().toString().trim();
+
+                UserRepository userRepository = new UserRepository(getApplicationContext());
+                User user = new User(New_id,New_password,New_name,New_surname);
+                generate_delete_dialog(user);
+            }
+        });
 
 
 
@@ -80,10 +97,49 @@ public class UpdateProfileActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
 
+
+    public void generate_delete_dialog(User user)
+    {
+        final User user_delete = user;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(UpdateProfileActivity.this); //do not write getapplicationContextg
+        builder.setTitle("WARNING");
+        builder.setMessage("Are you sure to delete ?" + "\n Name : " + user_delete.getID() + "\n Description : " + user_delete.getDescription());
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                delete_user(user_delete);
+                openLoginActivity();
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setIcon(android.R.drawable.ic_delete);
+
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
     public void openLoginActivity()
     {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // Animation entre écran
+    }
+
+    public void delete_user(final User user)
+    {
+        UserRepository userRepository = new UserRepository(getApplicationContext());
+        ListAndUserRepository listAndUserRepository = new ListAndUserRepository(getApplicationContext());
+
+        userRepository.DeleteTask(user);
+
     }
 }
