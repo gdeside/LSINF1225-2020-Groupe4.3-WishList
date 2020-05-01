67fa564c0c407c39468066fe1e1a62fcdf71b9ff
gdeside <guillaume.deside@student.uclouvain.be>
2020-05-01 10:07:49 +0200

    update update group and update friendship

--- Wishlist/app/src/main/java/com/example/wishlist/UpdateGroupActivity.java
+++ Wishlist/app/src/main/java/com/example/wishlist/UpdateGroupActivity.java
@@ -1,14 +1,27 @@
 package com.example.wishlist;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UpdateGroupActivity extends AppCompatActivity {
 
+    SearchView searcch_friend;
+    EditText up_group_name;
+    Button up_save;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_group);
+
+        searcch_friend=findViewById(R.id.up_group_seeach);
+        up_group_name=findViewById(R.id.up_namegroup);
+        up_save=findViewById(R.id.up_savegroup);
+
     }
 }
