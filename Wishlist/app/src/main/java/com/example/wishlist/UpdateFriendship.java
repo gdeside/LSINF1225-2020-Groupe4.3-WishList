57b551e7fead7a11d196487cae2d0c8b9cfd1fb0
gdeside <guillaume.deside@student.uclouvain.be>
2020-05-01 18:36:34 +0200

    update of application

--- Wishlist/app/src/main/java/com/example/wishlist/UpdateFriendship.java
+++ Wishlist/app/src/main/java/com/example/wishlist/UpdateFriendship.java
@@ -1,14 +1,27 @@
 package com.example.wishlist;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UpdateFriendship extends AppCompatActivity {
 
+
+    EditText up_nickname;
+    ImageButton deletebutton;
+    Button saveupdate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_friendship);
+
+        up_nickname=findViewById(R.id.up_friend_nickname);
+        deletebutton=findViewById(R.id.up_friend_delete);
+        saveupdate=findViewById(R.id.up_friend_save);
     }
+
 }
